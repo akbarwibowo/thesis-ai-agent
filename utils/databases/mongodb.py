922a07a518ac1efe6d4cef51d7eae4adef7e06f6
Putra Pamungkas Akbar Wibowo <akbarwibowo.2021@student.uny.ac.id>
2025-07-03 11:26:10 +0700

    FIX(mongodb): fix return value of the functions

--- utils/databases/mongodb.py
+++ utils/databases/mongodb.py
@@ -25,7 +25,7 @@ client = MongoClient(uri)
 database = client[database_name]
 
 
-def insert_documents(collection_name: str, documents: list):
+def insert_documents(collection_name: str, documents: list[dict]) -> bool:
     """Insert documents into a specified collection.
 
     Args:
@@ -33,8 +33,7 @@ def insert_documents(collection_name: str, documents: list):
         documents (list): A list of documents to insert into the collection.
 
     Returns:
-        bool: True if insertion is successful
-        str: error message string if failed.
+        bool: True if insertion is successful | str: error message string if failed.
     """
     try:
         logger.info(f"Inserting {len(documents)} documents into collection: {collection_name}")
@@ -45,18 +44,17 @@ def insert_documents(collection_name: str, documents: list):
     except Exception as e:
         error_msg = f"Error inserting documents: {str(e)}"
         logger.error(error_msg)
-        return error_msg
+        return False
 
 
-def retrieve_documents(collection_name: str):
+def retrieve_documents(collection_name: str) -> list[dict]:
     """Retrieve all documents from a specified collection.
 
     Args:
         collection_name (str): The name of the collection to retrieve documents from.
 
     Returns:
-        list: List of documents if successful
-        str: error message string if failed.
+        list (list): List of documents if successful | message (str): error message string if failed.
     """
     try:
         logger.info(f"Retrieving documents from collection: {collection_name}")
@@ -71,7 +69,7 @@ def retrieve_documents(collection_name: str):
     except Exception as e:
         error_msg = f"Error retrieving documents: {str(e)}"
         logger.error(error_msg)
-        return error_msg
+        return []
 
 
 def delete_collection(name: str):
@@ -93,6 +91,52 @@ def delete_collection(name: str):
     except Exception as e:
         error_msg = f"Error deleting collection: {str(e)}"
         logger.error(error_msg)
-        return error_msg
+        return False
     finally:
         client.close()
+
+if __name__ == "__main__":
+    # Example usage
+    collection_name = "bitcoin"
+    documents = [
+        {
+            "fundamental_data": {
+                "name": "Bitcoin",
+                "symbol": "BTC",
+                "market_cap": 800000000000,
+                "price": 40000,
+                "volume_24h": 30000000000,
+                "circulating_supply": 19000000,
+                "total_supply": 21000000
+            },
+            "prices": [
+                [
+                    1711843200000,
+                    69702.3087473573
+                ],
+                [
+                    1711929600000,
+                    71246.9514406015
+                ],
+                [
+                    1711983682000,
+                    68887.7495158568
+                ],
+            ]
+        }
+    ]
+    
+    # Insert documents
+    insert_result = insert_documents(collection_name, documents)
+    print(insert_result)
+    
+    # Retrieve documents
+    retrieve_result = retrieve_documents(collection_name)
+    print(retrieve_result)
+
+    fundamental_data = retrieve_result[0]["fundamental_data"]
+    print(f"Fundamental Data: {fundamental_data}")
+    
+    # Delete collection
+    delete_result = delete_collection(collection_name)
+    print(delete_result)
