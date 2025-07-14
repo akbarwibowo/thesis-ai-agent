a6070a7c44f80b65049595a6086ab90d3dcf0268
Putra Pamungkas Akbar Wibowo <akbarwibowo.2021@student.uny.ac.id>
2025-07-14 20:06:44 +0700

    FIX(na_agent_schema): modify schemas for na sub-graph

--- agents/schemas/na_agent_schema.py
+++ agents/schemas/na_agent_schema.py
@@ -1,11 +1,14 @@
 from typing import List, TypedDict, Annotated
+from pydantic import BaseModel, Field
 from operator import add
 
 
 
 class NAInputState(TypedDict):
     """the input of the narrative sub-graph"""
-    start_command: str
+    twitter_scrape_keywords: List[str]
+    twitter_scrape_max_tweets: int
+    cointelegraph_max_articles: int
 
 
 
@@ -16,14 +19,16 @@ class NAOutputState(TypedDict):
 
 
 
-class NAOverallState(TypedDict):
+class NAMapReducer(BaseModel):
+    """the map reducer for narrative data"""
+    summary: str = Field(description="The summary of the narrative documents")
+    quote: List[str] = Field(description="A direct quote from the narrative documents. Could be a single quote or multiple quotes.")
+    evidence_id: List[str | int] = Field(description="The evidence for the narrative documents")
+
+
+
+class NAOverallState(NAInputState, NAOutputState):
     """the overall state of the narrative sub-graph"""
     db_collection: str
-    documents: List[dict]
-
-
-
-class NAMapReducer(TypedDict):
-    """the map reducer for narrative data"""
-    reduce: Annotated[List[str], add]
-    evidence: Annotated[List[str | int], add]
+    chunked_documents: List[List[dict[str, str]]]
+    reduced_documents: List[NAMapReducer]
