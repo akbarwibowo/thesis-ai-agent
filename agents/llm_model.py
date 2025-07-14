a56c7d68f8571adcc8e0ee4a9db23b441efb3b29
Putra Pamungkas Akbar Wibowo <akbarwibowo.2021@student.uny.ac.id>
2025-07-14 21:35:27 +0700

    FIX(llm): add rate_limiter

--- agents/llm_model.py
+++ agents/llm_model.py
@@ -1,5 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import HumanMessage
+from langchain_core.rate_limiters import InMemoryRateLimiter
 
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
@@ -7,7 +7,14 @@ from os import getenv
 load_dotenv(find_dotenv())
 VERTEX_API_KEY = getenv("VERTEX_API_KEY")
 
+rate_limiter = InMemoryRateLimiter(
+    requests_per_second=1,  # <-- Super slow! We can only make a request once every 10 seconds!!
+    check_every_n_seconds=0.1,  # Wake up every 100 ms to check whether allowed to make a request,
+    max_bucket_size=20000,  # Controls the maximum burst size.
+)
+
 llm_model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     google_api_key=VERTEX_API_KEY,
+    rate_limiter=rate_limiter
 ) 
