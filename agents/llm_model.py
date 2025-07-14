c61402fc444f1a708ecb0776ad2f942c319e42a1
Putra Pamungkas Akbar Wibowo <akbarwibowo.2021@student.uny.ac.id>
2025-07-14 10:42:13 +0700

    FEAT(llm_model): add llm model file

---
+++ agents/llm_model.py
@@ -0,0 +1,13 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage
+
+from dotenv import load_dotenv, find_dotenv
+from os import getenv
+
+load_dotenv(find_dotenv())
+VERTEX_API_KEY = getenv("VERTEX_API_KEY")
+
+llm_model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=VERTEX_API_KEY,
+)
