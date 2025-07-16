f90985013fcbdfefacbe52b0ead5051f50bf598d
Putra Pamungkas Akbar Wibowo <akbarwibowo.2021@student.uny.ac.id>
2025-07-16 10:09:29 +0700

    FIX(ta_agent_schema): update schemas

--- agents/schemas/ta_agent_schema.py
+++ agents/schemas/ta_agent_schema.py
@@ -1,16 +1,28 @@
-from typing import TypedDict, List, Annotated
-from operator import add
+from typing import TypedDict, List
+from pydantic import BaseModel, Field
 
 
 
 class TAInputState(TypedDict):
-    """the input of the technical analysis sub-graph"""
-    start_command: str
+    """the input of the ta sub-graph"""
+    token_ids: List[str]
+
+
+
+class TAOutput(BaseModel):
+    """the output of the ta sub-graph"""
+    token_name: str = Field(description="The name of the token")
+    technical_analysis: str = Field(description="The technical analysis results")
+    evidence: List[str] = Field(description="The evidence supporting the analysis based on the technical data without duplicates")
 
 
 
 class TAOutputState(TypedDict):
-    """the output of the technical analysis sub-graph"""
-    token_name: Annotated[List[str], add]
-    technical_analysis: Annotated[List[str], add]
-    evidence: Annotated[List[str | int], add]
+    """the output state of the ta sub-graph"""
+    final_ta_report: List[TAOutput]
+
+
+
+class TAOverallState(TAInputState, TAOutputState):
+    """the overall state of the ta sub-graph"""
+    documents: List[dict]
