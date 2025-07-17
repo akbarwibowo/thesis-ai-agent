24bc42cb7384377e218ec1bb5f00509e0a0d1292
Putra Pamungkas Akbar Wibowo <akbarwibowo.2021@student.uny.ac.id>
2025-07-17 12:11:54 +0700

    FIX(main_agent_schema): update main schema

--- agents/schemas/main_agent_schema.py
+++ agents/schemas/main_agent_schema.py
@@ -1,7 +1,7 @@
-from typing import List, TypedDict, Annotated
-from operator import add
-from ta_agent_schema import TAOutputState
-from fa_agent_schema import FAOutputState
+from typing import List, TypedDict
+from pydantic import BaseModel, Field
+from ta_agent_schema import TAOutput
+from fa_agent_schema import FAOutput
 
 
 
@@ -9,9 +9,15 @@ class MainState(TypedDict):
     start_command: str
     token_ids: List[str]
     twitter_scrape_keywords: List[str]
+    identified_narratives: List[str]
     twitter_scrape_max_tweets: int
     cointelegraph_max_articles: int
-    final_na_report: str
-    final_ta_report: TAOutputState
-    final_fa_report: FAOutputState
+    final_na_report: str # populated by the NA graph
+    final_ta_report: List[TAOutput] # populated by the TA graph
+    final_fa_report: List[FAOutput] # populated by the FA graph
     final_analysis_report: str
+
+
+
+class NAIdentifierOutput(BaseModel):
+    narratives: List[str] = Field(description="Identified narrative(s) based on the analysis report")
